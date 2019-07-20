d3e97176aba725bb68697c260516a230abb59d21
Yudiño <5eduardoruano@gmail.com>
2019-07-19 22:45:12 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://KaiserSama:Ujvdyj8yjtY0yTjc@cluster0-ckbq4.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
